5b13cf8ac3fdc212d19c4ae103dcbe3c08460f4c
Mary Snegova <Marya0@yandex.ru>
2022-09-28 22:35:47 +0300

    feat: добавить поля email и password схеме User и контроллеру createUser

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const validator = require('validator');
 const User = require('../models/user');
 const {
   BAD_REQUEST,
@@ -32,8 +33,26 @@ module.exports.getUserById = (req, res) => {
     });
 };
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
+  if (!validator.isEmail(email)) {
+    res.status(BAD_REQUEST).send({
+      message: 'Передан невалидный email',
+    });
+    return;
+  }
+  User.create({
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  })
     .then((user) => res.json(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
--- models/user.js
+++ models/user.js
@@ -1,21 +1,30 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String, // имя — это строка
-    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
     minlength: 2, // минимальная длина имени — 2 символа
     maxlength: 30, // а максимальная — 30 символов
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
