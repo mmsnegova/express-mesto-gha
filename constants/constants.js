2bd900473abece37466153bceedfcebdf964fc3c
Mary Snegova <Marya0@yandex.ru>
2022-10-02 19:05:41 +0300

    delete status res constants

--- constants/constants.js
+++ constants/constants.js
@@ -1,13 +1,7 @@
-const BAD_REQUEST = 400;
-const NOT_FOUND = 404;
-const SERVER_ERROR = 500;
 const SALT_OR_ROUNDS = 10;
 const regex = /(https?:\/\/)([www.]?[a-zA-Z0-9-]+\.)([^\s]{2,})/;
 
 module.exports = {
-  BAD_REQUEST,
-  NOT_FOUND,
-  SERVER_ERROR,
   SALT_OR_ROUNDS,
   regex,
 };
